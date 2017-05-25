1c29b887bc449c9d386bac8e648fe9c116cfa60c
Sergey Q <bazzzooqa@gmail.com>
2017-05-25 08:40:53 +0500

    making snake moves

--- snake/Point.cs
+++ snake/Point.cs
@@ -59,5 +59,11 @@ namespace snake
         {
             return x + ", " + y + ", " + sym;
         }
+
+        public void Clear ()
+        {
+            sym = ' ';
+            Draw();
+        }
     }
 }
--- snake/Program.cs
+++ snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace snake
@@ -10,11 +11,7 @@ namespace snake
     {
         static void Main(string[] args)
         {
-			Point p = new Point(2, 4, '*');
-			Snake snk = new Snake(p, 4, Direction.RIGHT);
-			snk.Draw();
-
-            //making a box
+		    //making a box
             HorizontalLine hline1 = new HorizontalLine(0, 78, 0, '#');
             HorizontalLine hline2 = new HorizontalLine(0, 78, 23, '#');
             VerticalLine vline1 = new VerticalLine(0, 23, 0, '#');
@@ -23,6 +20,23 @@ namespace snake
             hline2.Draw();
             vline1.Draw();
             vline2.Draw();
+
+			Point p = new Point(2, 4, '*');
+			Snake snk = new Snake(p, 4, Direction.RIGHT);
+			snk.Draw();
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
+			Thread.Sleep(300);
+			snk.Move();
         }
     }
 }
